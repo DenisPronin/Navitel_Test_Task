6bb240b715287a7289384324e6322d2c0728d483
Denis <pronin.idenis@gmail.com>
2014-01-31 20:41:16 +0700

    в принципе все сделал

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#messages_inner_container").niceScroll({
+        horizrailenabled: false,
+        autohidemode: false,
+        cursorcolor: "#acb1c4",
+        background: "#f0f1f5"
+    });
+});
